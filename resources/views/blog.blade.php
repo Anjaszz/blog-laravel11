2e2004c6f9f5aa252696acbd8df8d5d1aef0638c
Anjaszz <anjasgans841@gmail.com>
2024-10-15 23:15:14 +0700

    init

---
+++ resources/views/blog.blade.php
@@ -0,0 +1,22 @@
+<x-layout>
+    <x-slot:title>{{ $title }}</x-slot:title>
+    <div class="container">
+        <div class="artikel">
+            <h2>Artikel 1</h2>
+            <img src="https://static.blog.ruangdeveloper.com/2021/12/mengenal-framework-laravel.png" alt="img1">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis vel explicabo nam.</p>
+        </div>
+
+        <div class="artikel">
+            <h2>Artikel 2</h2>
+            <img src="https://static.blog.ruangdeveloper.com/2021/12/mengenal-framework-laravel.png" alt="img2">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis vel explicabo nam.</p>
+        </div>
+
+        <div class="artikel">
+            <h2>Artikel 3</h2>
+            <img src="https://static.blog.ruangdeveloper.com/2021/12/mengenal-framework-laravel.png" alt="img3">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis vel explicabo nam.</p>
+        </div>
+    </div>
+</x-layout>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,4 @@
+<x-layout>
+  <x-slot:title>{{ $title }}</x-slot:title>
+  <h1>Test Home page</h1>
+</x-layout>
